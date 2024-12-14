d9a59ad9c86f01426259bed80a49115dab8e5d57
pepelev <kran440@gmail.com>
2024-12-14 19:46:40 +0500

    apply gnu lgpl

--- src/XL.Report/Styles/Fills/PatternFill.cs
+++ src/XL.Report/Styles/Fills/PatternFill.cs
@@ -1,4 +1,21 @@
-﻿namespace XL.Report.Styles.Fills;
+﻿#region Legal
+// Copyright 2024 Pepelev Alexey
+// 
+// This file is part of XL.Report.
+// 
+// XL.Report is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// XL.Report is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with XL.Report.
+// If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+namespace XL.Report.Styles.Fills;
 
 public sealed class PatternFill : Fill, IEquatable<PatternFill>
 {
--- src/XL.Report/Styles/ReadingOrder.cs
+++ src/XL.Report/Styles/ReadingOrder.cs
@@ -1,3 +1,20 @@
+#region Legal
+// Copyright 2024 Pepelev Alexey
+// 
+// This file is part of XL.Report.
+// 
+// XL.Report is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// XL.Report is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with XL.Report.
+// If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
 namespace XL.Report.Styles;
 
 public enum ReadingOrder : byte
